d3e8d46f7f553d1f85750169c834c41ce3ec23d9
Damien Labbe <damien.labbe@fms-ea.com>
2026-08-06 10:12:15 +0200

    ajout de la méhode generateChar

--- Minesweeper.java
+++ Minesweeper.java
@@ -1,4 +1,29 @@
+import java.util.List;
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Minesweeper {
+
+    public static char[][] generateChar( int rows , int cols, String difficult ){
+        char [] [] grid = new char[rows] [cols];
+        for(char[] row: grid){
+            Arrays.fill(row, ' ');
+        }
+        int dif = 0 if(difficult.equalsIgnoreCase("facile")) else 7;
+        int totalMines = rows * cols/8 + dif;
+        int placed = 0;
+
+        while (placed < totalMines) {
+            int r = ThreadLocalRandom.current().nextInt(0, rows);
+            int c = ThreadLocalRandom.current().nextInt(0, cols);
+            if (grid[r][c] != 'O') {
+                grid[r][c] = 'M';
+                placed++;
+            }
+        }
+
+        return grid;
+    }
     public static void main(String[] args) {
         if( args.length > 0) throw new IllegalArgumentException(" pas d'arguments");
     }
